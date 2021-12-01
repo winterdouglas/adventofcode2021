8765959e14f3ab58c2faec2dbbd2c1ef72a4140e
Douglas Winter <a.winterdoug@gmail.com>
2021-12-01 19:47:12 +0100

    Sonar sweep

---
+++ day1/sonarsweep/app.js
@@ -0,0 +1,31 @@
+import { createReadStream } from "fs";
+import { createInterface } from "readline";
+
+async function readMeasurements() {
+  const filename = "./measurements.txt";
+  let result = [];
+
+  const fileStream = createReadStream(filename, { encoding: "utf8" });
+  const rl = createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) {
+    result.push(parseInt(line, 10));
+  }
+
+  return result;
+}
+
+function determineLargerMeasurementCount(measurements) {
+  return measurements.reduce((accumulator, current, currentIndex, array) => {
+    if (currentIndex === 0) return 0;
+    const previous = array[currentIndex - 1];
+    return current > previous ? ++accumulator : accumulator;
+  }, 0);
+}
+
+const measurements = await readMeasurements();
+
+console.log(determineLargerMeasurementCount(measurements));
